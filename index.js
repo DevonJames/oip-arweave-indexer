8c48b392f32f4c6de063d897542f649ad14d6eaa
Devon R. James <devon@web3wg.org>
2024-12-17 13:19:01 -0800

    major bug and some cleanup and minor improvements
    - fixed a major bug that would cause it to re-index a somewhat random recent period over and over when a malformed temporary record was indexed if it was missing an inArweaveBlock value
    - cleaned up some handling on delete messages
    - added a new query parameter to getRecords: hasAudio (bool)
    - more improvements to the podcast-generator function (support for more historical figures, two of which are picked from it in the input from the end user. improved voice quality)
    - Scribes of Alexandria browser extension now takes a full screenshot of the page and passes the image to the scraping function
    - if author cannot be easily scraped from the HTML, the screenshot is sent through an image analyzer AI to find the article author's name, will also be able to be used to compare changes over time
    - more feedback on how synced its scraping+indexing of the blockchain is in the /records API response (and currently in the overly verbose console logs)

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const path = require('path');
 
 
 // Set higher body size limit (e.g., 10MB)
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // CORS configuration
 const corsOptions = {
