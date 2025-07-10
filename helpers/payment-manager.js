0bfb6a653732d38fc79b8414dd592958044e39c7
Devon R. James <devon@alexandria.io>
2025-07-10 13:30:07 -0700

    updated wallet dependency function

--- helpers/payment-manager.js
+++ helpers/payment-manager.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 const bitcoin = require('bitcoinjs-lib');
 const ecc = require('@bitcoinerlab/secp256k1');
-const bip32 = require('bip32');
+const { BIP32Factory } = require('bip32');
 const bip39 = require('bip39');
 
 bitcoin.initEccLib(ecc);
+const bip32 = BIP32Factory(ecc);
 
 class PaymentManager {
     constructor() {
