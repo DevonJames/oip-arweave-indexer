3800fc3b3eea2421190b6028680f8c73fbb0add2
Devon R. James <devon@alexandria.io>
2025-07-14 13:49:00 -0700

    updated workout and associatedURLOnWeb templates in templates.config.js

--- config/templates.config.js
+++ config/templates.config.js
@@ -5,7 +5,7 @@ module.exports = {
     defaultTemplates: {
         basic: "-9DirnjVO1FlbEW1lN8jITBESrTsQKEM_BoZ1ey_0mk",
         creatorRegistration: "BKVvTSXmmJni-L82irZfPFXWWJLBcdvbxS34jP1FTG8", 
-        associatedUrlOnWeb: "_RJtiNMr1Ls9NG3G9-6rvbI5Sn0ccSnRTnrQCZi3ABE",
+        associatedUrlOnWeb: "eVTn36AY0JdLI_jzj9FivxrxtvlkVQIPI5ma-1x0E2c",
         text: "of_eh0kGy65uYD0xsTh7Qe6JVTfJ2oFoA7xp4H7t7YI",
         image: "AkZnE1VckJJlRamgNJuIGE7KrYwDcCciWOMrMh68V4o",
         audio: "9MrI8kJvBJ-u7uvgngMZ-gMo5RREDwey8G7mJEUOpdo",
@@ -15,7 +15,7 @@ module.exports = {
         jfkFilesPageOfDocument: "WP-tZ5nwxrMwr5wKHazTFUtGLRd6nQWRBl8E63Kqufg",
         jfkFilesDocument: "d3xoxXQ8hUlrgKTo1UZc4_GqaKTaApJs0tLFuK6Hxqk",
         exercise: "XVu78TY-4LX6-vOajc7AKAk9jn1amFSC87XMTGTz4Mw",
-        workout: "T_16JOpyRKt0dmuZikHPpDcIhwh4DR1ZspHU4BKa-qE",
+        workout: "yxguXXKD_YSnCjC1ILYFBmQ6F20F5dKOoA6hxuqhNUk",
         recipe: "SLsJ91-Z82rRBPkDrZlG87aIpbw6zOlmK96nh5uf6G4",
         nutritionalInfo: "_xuiAaVKxPL1Dsp-AceCpXqnc0C_WwHC-Ws1vN-cBUs",
         podcastShow: "JQ_gsZucnD5aM9V9ue748R5COGFyOZYstGVSm1iX7rU",
